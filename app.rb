d3efdfef71d3a479ca624c1e38dedaf81d6ea553
Student <katierodgers95@gmail.com>
2020-01-06 16:58:49 +0000

    added shotgun

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
     "Hello World!"
+end
+
+get '/secret' do
+    "Shh, it's a secret!"
+end
+
+get '/hungry' do
+    "Let's get some food"
 end
